4c85982294d35b4526ecc2f792ac4686178348aa
monta <ellih>
2017-11-07 06:29:37 +0000

    added monopoly deal page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   
   root "pages#homepage"
   get 'about', to: 'pages#about'
+  
+  get 'pages/monopoly_go_rigged', to: 'pages#monopoly_go_rigged'
+  
+  resources :articles
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
